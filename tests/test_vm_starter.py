7423a786753f12d007d55083c8982a489833b682
Alexander Bush <ghostkaa@gmail.com>
2024-05-05 00:10:48 +0300

    reorder tests

--- tests/test_vm_starter.py
+++ tests/test_vm_starter.py
@@ -6,7 +6,7 @@ import pytest
 from pxpilot.pxtool import VMService, VirtualMachine
 from pxpilot.pxtool.models import VMType, VMState
 from pxpilot.vm_management.host_validator import HostValidator
-from pxpilot.vm_management.models import VMContext, StartStatus, VMLaunchSettings
+from pxpilot.vm_management.models import VMContext, StartStatus, VMLaunchSettings, StartResult
 from pxpilot.vm_management.vm_starter import VMStarter
 
 
@@ -44,49 +44,54 @@ def flow_item():
     return context
 
 
-def test_start(mock_logger, mock_vm_service):
-    starter = VMStarter(mock_vm_service, mock_host_validator)
-    with patch.object(starter, '_start_vm_and_wait'):
-        starter.start(flow_item())
+class TestStartMethod:
+    def test_start_executes_successfully(self, mock_logger1, mock_vm_service, mock_host_validator):
+        starter = VMStarter(mock_vm_service, mock_host_validator)
+        with patch.object(starter, '_start_vm_and_wait', return_value=StartResult(status=StartStatus.STARTED)) as mock_start_wait:
+            result = starter.start(flow_item())
+
+            assert result.status == StartStatus.STARTED
+            mock_start_wait.assert_called_once()
+
+    def test_launch_settings_missing_returns_info_missing(self, mock_logger1, mock_vm_service, mock_host_validator):
+        context = VMContext(vm_id=100, status=StartStatus.UNKNOWN,
+                            vm_info=VirtualMachine(vm_id=100, vm_type=VMType.LXC, node="test", name="test",
+                                                   status=VMState.STOPPED),
+                            vm_launch_settings=None)
+
+        starter = VMStarter(mock_vm_service, mock_host_validator)
+        with patch.object(starter, '_start_vm_and_wait', return_value=None) as mock_start_wait:
+            result = starter.start(context)
+
+            assert result.status == StartStatus.INFO_MISSED
+            mock_start_wait.assert_not_called()
+
+    def test_launch_disabled_returns_disabled(self, mock_logger1, mock_vm_service, mock_host_validator):
+        context = VMContext(vm_id=100, status=StartStatus.UNKNOWN,
+                            vm_info=VirtualMachine(vm_id=100, vm_type=VMType.LXC, node="test", name="test",
+                                                   status=VMState.STOPPED),
+                            vm_launch_settings=VMLaunchSettings(vm_id=100, node="test", enabled=False))
+
+        starter = VMStarter(mock_vm_service, mock_host_validator)
+        with patch.object(starter, '_start_vm_and_wait', return_value=None) as mock_start_wait:
+            result = starter.start(context)
+
+            assert result.status == StartStatus.DISABLED
+            mock_start_wait.assert_not_called()
+
+    def test_already_started_returns_already_started(self, mock_logger1, mock_vm_service, mock_host_validator):
+        context = VMContext(vm_id=100, status=StartStatus.STARTED,
+                            vm_info=VirtualMachine(vm_id=100, vm_type=VMType.LXC, node="test", name="test",
+                                                   status=VMState.RUNNING),
+                            vm_launch_settings=VMLaunchSettings(vm_id=100, node="test"))
+
+        starter = VMStarter(mock_vm_service, mock_host_validator)
+        with patch.object(starter, '_start_vm_and_wait', return_value=None) as mock_start_wait:
+            result = starter.start(context)
+
+            assert result.status == StartStatus.ALREADY_STARTED
+            mock_start_wait.assert_not_called()
 
 
-def test_start_start_info_missed(mock_logger1, mock_vm_service, mock_host_validator):
-    context = VMContext(vm_id=100, status=StartStatus.UNKNOWN,
-                        vm_info=VirtualMachine(vm_id=100, vm_type=VMType.LXC, node="test", name="test",
-                                               status=VMState.STOPPED),
-                        vm_launch_settings=None)
-
-    starter = VMStarter(mock_vm_service, mock_host_validator)
-    with patch.object(starter, '_start_vm_and_wait', return_value=None) as mock_start_wait:
-        result = starter.start(context)
-
-        assert result.status == StartStatus.INFO_MISSED
-        mock_start_wait.assert_not_called()
-
-
-def test_start_disabled(mock_logger1, mock_vm_service, mock_host_validator):
-    context = VMContext(vm_id=100, status=StartStatus.UNKNOWN,
-                        vm_info=VirtualMachine(vm_id=100, vm_type=VMType.LXC, node="test", name="test",
-                                               status=VMState.STOPPED),
-                        vm_launch_settings=VMLaunchSettings(vm_id=100, node="test", enabled=False))
-
-    starter = VMStarter(mock_vm_service, mock_host_validator)
-    with patch.object(starter, '_start_vm_and_wait', return_value=None) as mock_start_wait:
-        result = starter.start(context)
-
-        assert result.status == StartStatus.DISABLED
-        mock_start_wait.assert_not_called()
-
-
-def test_start_already_runned(mock_logger1, mock_vm_service, mock_host_validator):
-    context = VMContext(vm_id=100, status=StartStatus.STARTED,
-                        vm_info=VirtualMachine(vm_id=100, vm_type=VMType.LXC, node="test", name="test",
-                                               status=VMState.RUNNING),
-                        vm_launch_settings=VMLaunchSettings(vm_id=100, node="test"))
-
-    starter = VMStarter(mock_vm_service, mock_host_validator)
-    with patch.object(starter, '_start_vm_and_wait', return_value=None) as mock_start_wait:
-        result = starter.start(context)
-
-        assert result.status == StartStatus.ALREADY_STARTED
-        mock_start_wait.assert_not_called()
+class TestStartAndWaitMethod:
+    pass

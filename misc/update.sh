58b7fe2b07929778e4c91db380b5889a7019c39a
Alexander Bush <ghostkaa@gmail.com>
2024-05-12 21:32:23 +0300

    fix: update.sh

--- misc/update.sh
+++ misc/update.sh
@@ -4,13 +4,13 @@
 # License: MIT
 # https://github.com/ghostkaa/pxpilot/raw/main/LICENSE
 
-VERSION="0.1.3"
+VERSION="0.1.4"
 GITHUB_REPO="https://github.com/ghostkaa/pxpilot"
 RELEASE_URL="$GITHUB_REPO/archive/refs/tags/v$VERSION.tar.gz"
 TAR_FILE="pxpilot.tar.gz"
 PROJECT_DIR="pxpilot"
 BACKUP_DIR="${PROJECT_DIR}_backup"
-UPDATE_DIR="pxpilot"
+UPDATE_DIR="pxpilot_update"
 
 echo "Downloading the latest release from $RELEASE_URL ..."
 wget "$RELEASE_URL" -O $TAR_FILE
@@ -21,11 +21,11 @@ if [ -d "$UPDATE_DIR" ]; then
 fi
 mkdir $UPDATE_DIR
 
-echo "Extracting the downloaded file..."
+echo "Extracting the downloaded file to $UPDATE_DIR..."
 tar -xzf $TAR_FILE --strip-components=1 -C $UPDATE_DIR
 
-echo "Copying config file..."
-cp -f $PROJECT_DIR/config.yml $UPDATE_DIR/config.yml
+echo "Copying config file from $PROJECT_DIR to $UPDATE_DIR..."
+cp -f $PROJECT_DIR/config.yaml $UPDATE_DIR/config.yaml
 
 if [ -d "$BACKUP_DIR" ]; then
     echo "Removing existing directory $BACKUP_DIR..."
@@ -34,3 +34,4 @@ fi
 
 mv $PROJECT_DIR $BACKUP_DIR
 mv $UPDATE_DIR $PROJECT_DIR
+echo "All done"

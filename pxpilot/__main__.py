247d5bb14d50b2047d48ad036fa8eeb95d009c08
Alexander Bush <ghostkaa@gmail.com>
2024-05-13 18:51:03 +0300

    init config validation

--- pxpilot/__main__.py
+++ pxpilot/__main__.py
@@ -1,4 +1,15 @@
+import argparse
+
 from pxpilot.pilot import main
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--validate_config", action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+
+    if args.validate_config is None:
+        main()
+        exit(0)
+
+    print("Validate config")
+    pass

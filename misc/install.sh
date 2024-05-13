a32806fa820bf8de402e8ccbe70a9f196c29f9c5
Alexander Bush <ghostkaa@gmail.com>
2024-05-13 14:39:29 +0300

    ops: use latest in install and update scripts

--- misc/install.sh
+++ misc/install.sh
@@ -4,15 +4,35 @@
 # License: MIT
 # https://github.com/ghostkaa/pxpilot/raw/main/LICENSE
 
-#VERSION="0.1.4"
 GITHUB_REPO="https://github.com/ghostkaa/pxpilot"
-#RELEASE_URL="$GITHUB_REPO/archive/refs/tags/v$VERSION.tar.gz"
 TAR_FILE="pxpilot.tar.gz"
 UNZIP_DIR="pxpilot"
 
 echo "Downloading the latest release..."
-bash download_latest.sh $GITHUB_REPO $TAR_FILE
-#wget "$RELEASE_URL" -O $TAR_FILE
+
+LATEST_RELEASE_URL=$(wget -S --spider "$GITHUB_REPO"/releases/latest 2>&1 | grep "Location:" | tail -n 1 | awk '{print $2}' | tr -d '\r')
+
+if [ -z "$LATEST_RELEASE_URL" ]; then
+    echo "Failed to retrieve the latest release URL."
+    exit 1
+fi
+
+VERSION=$(echo "$LATEST_RELEASE_URL" | grep -oP 'tag/\K[^/]*')
+
+if [ -z "$VERSION" ]; then
+    echo "Failed to parse version from URL: $LATEST_RELEASE_URL"
+    exit 1
+fi
+
+ARCHIVE_URL="$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz"
+
+echo "Downloading file $ARCHIVE_URL..."
+wget "$ARCHIVE_URL" -O "$TAR_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to download the archive."
+    exit 1
+fi
 
 mkdir $UNZIP_DIR
 
--- misc/update.sh
+++ misc/update.sh
@@ -12,8 +12,30 @@ BACKUP_DIR="${PROJECT_DIR}_backup"
 UPDATE_DIR="pxpilot_update"
 
 echo "Downloading the latest release..."
-bash download_latest.sh $GITHUB_REPO $TAR_FILE
-#wget "$RELEASE_URL" -O $TAR_FILE
+
+LATEST_RELEASE_URL=$(wget -S --spider "$GITHUB_REPO"/releases/latest 2>&1 | grep "Location:" | tail -n 1 | awk '{print $2}' | tr -d '\r')
+
+if [ -z "$LATEST_RELEASE_URL" ]; then
+    echo "Failed to retrieve the latest release URL."
+    exit 1
+fi
+
+VERSION=$(echo "$LATEST_RELEASE_URL" | grep -oP 'tag/\K[^/]*')
+
+if [ -z "$VERSION" ]; then
+    echo "Failed to parse version from URL: $LATEST_RELEASE_URL"
+    exit 1
+fi
+
+ARCHIVE_URL="$GITHUB_REPO/archive/refs/tags/$VERSION.tar.gz"
+
+echo "Downloading file $ARCHIVE_URL..."
+wget "$ARCHIVE_URL" -O "$TAR_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to download the archive."
+    exit 1
+fi
 
 if [ -d "$UPDATE_DIR" ]; then
     echo "Removing existing directory $UPDATE_DIR..."

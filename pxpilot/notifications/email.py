cfa3edb94a8b85643c51db5b8528609a7da53546
Alexander Bush <ghostkaa@gmail.com>
2024-05-19 21:06:40 +0300

    feat: send email notifications

--- pxpilot/notifications/email.py
+++ pxpilot/notifications/email.py
@@ -1,9 +1,25 @@
+import smtplib
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from .log import LOGGER
-from .notifications import NotificationMessage, Notifier
+from .notifications import NotificationMessage, Notifier, ProxmoxMessage
+from pxpilot.__about__ import __title__
 
 
-class EmailMessage(NotificationMessage):
-    pass
+class EmailMessage(ProxmoxMessage):
+    def __init__(self):
+        super().__init__()
+        self.subject = f"{__title__} - "
+
+    def add_header(self, start_time: datetime):
+        self.subject += f"Proxmox VMs Startup Summary on {start_time.strftime('%d-%b-%Y')}"
+        super().add_header(start_time)
+
+    def fatal(self, error_message: str) -> None:
+        self.subject = f"{__title__} - Proxmox VMs Startup Failed on {datetime.now().now().strftime('%d-%b-%Y')}"
+        super().fatal(error_message)
 
 
 class EmailNotifier(Notifier):
@@ -14,4 +30,23 @@ class EmailNotifier(Notifier):
         return EmailMessage()
 
     def send(self, message: NotificationMessage):
+        msg = MIMEMultipart()
+        msg["From"] = self._config["from_email"]
+        msg["To"] = self._config["to_email"]
+        msg["Subject"] = message.subject if isinstance(message, EmailMessage) else __title__
+
+        msg.attach(MIMEText(message.message.replace('\n', '<br>'), 'html'))
+
+        try:
+            smtp = smtplib.SMTP(self._config["smtp_server"], self._config["smtp_port"])
+            smtp.starttls()
+            smtp.login(self._config["smtp_user"], self._config["smtp_password"])
+
+            smtp.send_message(msg)
+
+        except Exception as ex:
+            LOGGER.error(f"{ex}")
+        finally:
+            smtp.quit()
+
         LOGGER.info("Email has been send successfully.")

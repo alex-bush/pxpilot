0180f082414734d5abcc131c62db10231d4c127c
ghostkaa <ghostkaa@gmail.com>
2024-07-07 20:42:47 +0300

    feat(ui): small refactoring

--- src/frontend/src/components/VmStartupOptionsModal.jsx
+++ src/frontend/src/components/VmStartupOptionsModal.jsx
@@ -38,7 +38,7 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
             !healthcheckEnabled
             || healthcheckEnabled
             && data.healthcheck.check_method
-            && data.healthcheck.check_method !== 'none'
+            && data.healthcheck.check_method !== defaultCheck
             && data.healthcheck.target_url
         );
         setIsOkDisabled(!isValid);
@@ -46,12 +46,18 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
 
     useEffect(() => {
         fetchAllVirtualMachines().then(vms => {
-            let d = vms.map(item => {return { value: item.id, label: item.id + ': ' + item.name, disabled: usedKeys && usedKeys.findIndex(i => i === item.id) > -1 }} )
+            let d = vms.map(vm => {
+                return {
+                    value: vm.id,
+                    label: vm.id + ': ' + vm.name,
+                    disabled: usedKeys && usedKeys.findIndex(i => i === vm.id) > -1
+                }
+            })
             setAvailableVms(d);
         })
     }, [usedKeys])
 
-    const handleOk = () => {
+    const handleOkClick = () => {
         if (!data.healthcheck || data.healthcheck.check_method === defaultCheck) {
             onOk(key, {...data, healthcheck: null});
         } else {
@@ -61,13 +67,13 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
         setData({healthcheck: {check_method: defaultCheck}});
     };
 
-    const handleCancel = () => {
+    const handleCancelClick = () => {
         onCancel();
         setData({healthcheck: {check_method: defaultCheck}});
     };
 
-    function handleHealthCheckChange(value) {
-        if (value === 'none') {
+    function handleHealthcheckSelectChange(value) {
+        if (value === defaultCheck) {
             setHealthcheckEnabled(false);
             setData({
                 ...data, healthcheck: {check_method: defaultCheck}
@@ -80,7 +86,7 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
         }
     }
 
-    function handleVmChange(value) {
+    function handleVmSelectChange(value) {
         console.log(value)
         setData({...data, vm_id: value});
     }
@@ -90,25 +96,24 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
             title="Add VM to startup list"
             centered
             open={isModalOpen}
-            onOk={handleOk}
-            onCancel={handleCancel}
+            onOk={handleOkClick}
+            onCancel={handleCancelClick}
             okButtonProps={{disabled: isOkDisabled}}>
             <Card>
                 <div className='flex gap-4 w-full items-end'>
-                <LabeledTextField
-                    title='VM Id'
-                    type='number'
-                    value={data.vm_id}
-                    onChange={(value) => setData({...data, vm_id: value})}/>
+                    <LabeledTextField
+                        title='VM Id'
+                        type='number'
+                        value={data.vm_id}
+                        onChange={(value) => setData({...data, vm_id: value})}/>
                     <Select
-                        // defaultValue={defaultCheck}
                         value={data.vm_id}
                         className='w-2/5'
                         size='large'
                         placeholder="Select a VM"
                         options={availableVms}
                         allowClear
-                        onChange={handleVmChange}
+                        onChange={handleVmSelectChange}
                     />
                 </div>
                 <LabeledTextField
@@ -121,20 +126,26 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
                     title='Description'
                     value={data.description}
                     onChange={(value) => setData({...data, description: value})}/>
-                <Divider />
+                <Divider/>
                 <CheckboxField
                     className='mt-3'
                     title='Wait for the virtual machine to finish starting'
                     value={data.startup_parameters?.await_running}
-                    onChange={(e) => setData({...data, startup_parameters: {...data.startup_parameters, await_running: e.target.checked}})}/>
+                    onChange={(e) => setData({
+                        ...data,
+                        startup_parameters: {...data.startup_parameters, await_running: e.target.checked}
+                    })}/>
                 <LabeledTextField
                     className='mt-3'
                     title='Timeout'
                     type='number'
                     value={data.startup_timeout ?? 120} disabled={!data.startup_parameters?.await_running}
-                    onChange={(value) => setData({...data, startup_parameters: {...data.startup_parameters, startup_timeout: value}})}/>
+                    onChange={(value) => setData({
+                        ...data,
+                        startup_parameters: {...data.startup_parameters, startup_timeout: value}
+                    })}/>
 
-                <Divider />
+                <Divider/>
                 <div className='health-check'>
 
                     <div className='flex gap-4 w-full mt-3'>
@@ -145,7 +156,7 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
                             className='w-full'
                             options={healthCheckTypes}
                             allowClear
-                            onChange={handleHealthCheckChange}
+                            onChange={handleHealthcheckSelectChange}
                         />
                     </div>
 
@@ -161,10 +172,15 @@ export default function VmStartupOptionsModal({isModalOpen, item, usedKeys, onOk
                         />
                     </div>
                 </div>
-                <Divider />
-                <div>
 
-                </div>
+                {data?.dependencies?.length > 0 && (
+                    <div>
+                        <Divider/>
+                        <span>Depends on: </span>
+                        {data.dependencies?.map(d => (
+                            <span key={d}>{d}</span>
+                        ))}
+                    </div>)}
             </Card>
         </Modal>
     </>)
